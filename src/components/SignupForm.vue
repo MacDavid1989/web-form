2fc182f96626ff3a8092c1004a6373f24b9e391b
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-24 15:36:32 -0400

    listen for a submit event with the prevent modifier and invoke handleSubmit method, carryout simple validation on password length and only print to console if validation passes, provide error message for validation

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" required v-model="email" />
 
     <label>Password:</label>
     <input type="password" required v-model="password" />
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -27,6 +28,10 @@
       <input type="checkbox" required v-model="terms" />
       <label>Accpet Terms and Conditions</label>
     </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
+    </div>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
@@ -45,6 +50,7 @@ export default {
       terms: false,
       tempSkill: "",
       skills: [],
+      passwordError: "",
     };
   },
   methods: {
@@ -56,6 +62,21 @@ export default {
     },
     deleteSkill(index) {
       this.skills.splice(index, 1);
+    },
+    handleSubmit() {
+      // validate password
+      this.passwordError =
+        this.password.length > 5
+          ? ""
+          : "Password must be at least 6 chars long";
+
+      if (!this.passwordError) {
+        console.log("email", this.email);
+        console.log("password", this.password);
+        console.log("role", this.role);
+        console.log("skills", this.skills);
+        console.log("terms", this.terms);
+      }
     },
   },
 };
@@ -107,5 +128,22 @@ input[type="checkbox"] {
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: #ff0062;
+  font-size: 0.8em;
+  margin-top: 10px;
+  font-weight: bold;
 }
 </style>
